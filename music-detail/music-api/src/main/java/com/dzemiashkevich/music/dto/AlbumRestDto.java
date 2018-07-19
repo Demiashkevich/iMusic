02aed39b9f4da55e77836bff20feed9bf1fab3d5
Yahor Dzemiashkevich <yahor_dzemiashkevich@epam.com>
2018-07-19 18:02:23 +0300

    Add music rest dto

---
+++ music-detail/music-api/src/main/java/com/dzemiashkevich/music/dto/AlbumRestDto.java
@@ -0,0 +1,120 @@
+package com.dzemiashkevich.music.dto;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
+import java.time.LocalDate;
+
+public class AlbumRestDto {
+
+    private String artistName;
+
+    private String contentRating;
+
+    private String copyright;
+
+    private Boolean isComplete;
+
+    private Boolean isSingle;
+
+    private String name;
+
+    private String recordLabel;
+
+    private LocalDate releaseDate;
+
+    private Integer trackCount;
+
+    private String url;
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public String getContentRating() {
+        return contentRating;
+    }
+
+    public void setContentRating(String contentRating) {
+        this.contentRating = contentRating;
+    }
+
+    public String getCopyright() {
+        return copyright;
+    }
+
+    public void setCopyright(String copyright) {
+        this.copyright = copyright;
+    }
+
+    public Boolean getComplete() {
+        return isComplete;
+    }
+
+    public void setComplete(Boolean complete) {
+        isComplete = complete;
+    }
+
+    public Boolean getSingle() {
+        return isSingle;
+    }
+
+    public void setSingle(Boolean single) {
+        isSingle = single;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRecordLabel() {
+        return recordLabel;
+    }
+
+    public void setRecordLabel(String recordLabel) {
+        this.recordLabel = recordLabel;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public Integer getTrackCount() {
+        return trackCount;
+    }
+
+    public void setTrackCount(Integer trackCount) {
+        this.trackCount = trackCount;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return EqualsBuilder.reflectionEquals(this, o);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+}
